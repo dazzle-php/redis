4b450cd756ed1b9e42e9448f1dd02f76f9e0756b
halobangbangboom <tianchen_cc@yeah.net>
2017-07-01 21:05:09 +0800

    Non-issue : Commands of orted sets implemented

--- example/example.php
+++ example/example.php
@@ -11,17 +11,14 @@ $client = new Client('192.168.99.100:32768', $loop);
 
 $ret = [];
 
+$client->select(0);
+
 $client->flushDb();
 
-$client->set('test','Hello Kraken Redis!')->then(function ($value) {
-    global $ret;
-    $ret[] = $value;
-    echo $value.PHP_EOL;
-});
+$client->set('test','Hello Kraken Redis!');
 
-$client->get('test')->then(function ($value) {
-    global $ret;
-    $ret[] = $value;
+$client->exists('test')->then(function ($response) {
+    echo $response.PHP_EOL;
 });
 
 $client->info(['cpu'])->then(function ($value) {
@@ -29,16 +26,31 @@ $client->info(['cpu'])->then(function ($value) {
     $ret[] = $value;
 });
 
+$client->zAdd('k', [], ['0.002' => 'h','0.004' => 'l']);
+
+$client->zRange('k', 0, -1, true)->then(function ($response) {
+    var_export($response);
+    echo PHP_EOL;
+});
+
+$client->zRemRangeByScore('k',0,1)->then(function ($response) {
+    echo $response.PHP_EOL;
+});
+
+
+$client->quit();
+
 $loop->onStart(function () use ($client, $loop) {
+
     $client->dispatcher->on('error', function(\Exception $e) {
         echo $e->getMessage().PHP_EOL;
     });
+
     $client->dispatcher->on('close', function () use ($loop){
         $loop->stop();
     });
+
     $client->connect();
 });
 
 $loop->start();
-
-var_export($ret);
