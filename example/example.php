e09df0e5d0b345415976d97e3b0145463691f0be
halobangbangboom <tianchen_cc@yeah.net>
2017-07-03 23:06:30 +0800

    Update example\example.php

--- example/example.php
+++ example/example.php
@@ -1,66 +1,103 @@
 <?php
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
 use Dazzle\Loop\Model\SelectLoop;
 use Dazzle\Loop\Loop;
 use Dazzle\Redis\Redis;
-
 $loop = new Loop(new SelectLoop());
-
-// $redis = new Redis('tcp://127.0.0.1:6379', $loop);
-
+// $redis = new Redis('192.168.99.100:32769', $loop);
+$redis = new Redis('tcp://127.0.0.1:6379', $loop);
 $ret = [];
-
-$loop = new Loop(new SelectLoop());
-
-$redis = new Redis('192.168.99.100:32768', $loop);
-
-$ret = [];
-
-$redis->select(0);
-
-$redis->flushDb();
-
-$redis->set('test','Hello Kraken Redis!');
-
-$redis->exists('test')->then(function ($response) {
-    echo $response.PHP_EOL;
-});
-
-$redis->info(['cpu'])->then(function ($value) {
-    global $ret;
-    $ret[] = $value;
-});
-
+$redis->on('start', function(Redis $redis) {
+    $redis->flushDb()->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->set('test1','test1');
+    $redis->set('test2','test2');
+    $redis->set('test','Hello Dazzle Redis!')->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->get('test')->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->append('test', 'Make PHP Awesome')->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->get('test')->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->ping()->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->exists('test','test1','test2')->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->bitPos('test',5,10,20)->then(function ($value) {
+        //todo : fix
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->expire('test',1)->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->info(['cpu'])->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->dump('test')->then(function ($value) use ($redis) {
+        global $ret;
+        $ret[] = $value;
+        $redis->restore('test',0,$value)->then(function ($value) {
+            global $ret;
+            $ret[] = $value;
+        });
+    });
     $redis->touch('f','f1','f2','f3')->then(function ($value) {
         global $ret;
         $ret[] = $value;
     });
-});
-
-$redis->zRange('k', 0, -1, true)->then(function ($response) {
-    var_export($response);
-    echo PHP_EOL;
-});
-
-$redis->zRemRangeByScore('k',0,1)->then(function ($response) {
-    echo $response.PHP_EOL;
-});
-
-$redis->quit();
-
-$loop->onStart(function () use ($redis, $loop) {
-
-    $redis->dispatcher->on('error', function(\Exception $e) {
-        echo $e->getMessage().PHP_EOL;
+    $redis->rename('test','new_test')->then(function ($value) use ($redis) {
+       if ($value == 'OK') {
+           global $ret;
+           $ret[] = 'RENAME OK';
+       }
     });
-
-    $redis->dispatcher->on('close', function () use ($loop){
-        $loop->stop();
+    $redis->ttl('new_test')->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
     });
-
-    $redis->connect();
+    $redis->del('test','test1','test2')->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->randomKey()->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->type('test1')->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->unLink('new_test')->then(function ($value) {
+        global $ret;
+        $ret[] = $value;
+    });
+    $redis->end();
 });
-
+$redis->on('stop', function(Redis $redis) use(&$ret) {
+    var_export($ret);
+});
+$redis->on('error', function($ex) {
+    var_dump($ex);
+});
+$loop->onTick(function() use($redis) {
+    $redis->start();
+});
 $loop->start();
