597faea7a518fe84db5f7d1c1a4b24cb1a7066a4
halobangbangboom <tianchen_cc@yeah.net>
2017-07-09 21:34:34 +0800

    Create APIs test files

---
+++ test/TModule/Command/RedisApiListTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dazzle\Redis\Test\TModule\Command;
+
+use Dazzle\Promise\Promise;
+use Dazzle\Redis\Redis;
+use Dazzle\Redis\RedisInterface;
+use Dazzle\Redis\Test\TModule;
+use Dazzle\Redis\Test\TModule\_Support\RedisTrait;
+
+class RedisApiListTest extends TModule
+{
+    use RedisTrait;
+
+    /**
+     * @group ignored
+     * @dataProvider redisProvider
+     * @param RedisInterface $redis
+     */
+    public function testRedis_(RedisInterface $redis)
+    {
+       
+    }
+}
